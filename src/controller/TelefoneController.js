ed037d54a150043b4dced4d6beb8e8a7ecb42408
Leonardo Soares <leo.soares64@outlook.com>
2020-05-26 19:00:42 -0300

    Update TelefoneController.js

--- src/controller/TelefoneController.js
+++ src/controller/TelefoneController.js
@@ -2,6 +2,6 @@ const db = require("../config/database");
 
 // ==> Método responsável por listar indicadores de telefonia:
 exports.listAll = async (req, res) => {
-    const response = await db.query('SELECT * FROM perf_unificado.vwm_perf_fone_dia WHERE data = current_date');
+    const response = await db.query('SELECT * FROM tbl_teste');
     res.status(200).send(response.rows);
 };
